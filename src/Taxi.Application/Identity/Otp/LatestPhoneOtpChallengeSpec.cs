164f4a98b8ca55dd3c79abd485a21e04195341e0
spiderams <ayanleh.moussasaid@gmail.com>
2026-07-21 18:11:42 -0400

    Faire les tests unitaire Otp

--- src/Taxi.Application/Identity/Otp/LatestPhoneOtpChallengeSpec.cs
+++ src/Taxi.Application/Identity/Otp/LatestPhoneOtpChallengeSpec.cs
@@ -3,11 +3,17 @@ using Taxi.Domain.Identity;
 
 namespace Taxi.Application.Identity.Otp;
 
-internal sealed class LatestPhoneOtpChallengeSpec : Specification<PhoneOtpChallenge>
+public sealed class LatestPhoneOtpChallengeSpec : Specification<PhoneOtpChallenge>
 {
     public LatestPhoneOtpChallengeSpec(string phoneNumber)
     {
         Query.Where(c => c.PhoneNumber == phoneNumber)
+            .OrderByDescending(c => c.CreatedAt)
+            .Take(1);
+    }
+    public LatestPhoneOtpChallengeSpec(string phoneNumber, string purpose)
+    {
+        Query.Where(c => c.PhoneNumber == phoneNumber && c.Purpose == purpose)
             .OrderByDescending(c => c.CreatedAt)
             .Take(1);
     }
